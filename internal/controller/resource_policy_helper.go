8cef435628e546e2850dac002e5a84579881563d
Leonardo Milleri <lmilleri@redhat.com>
2025-09-25 11:19:55 +0100

    Generate resource policy
    Signed-off-by: Leonardo Milleri <lmilleri@redhat.com>

---
+++ internal/controller/resource_policy_helper.go
@@ -0,0 +1,44 @@
+/*
+Copyright Confidential Containers Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"os"
+)
+
+// generateResourcePolicyRego generates the Rego policy content based on profile type
+func generateResourcePolicyRego(profileType string) (string, error) {
+	var templateFile string
+
+	// Select template file based on profile type
+	switch profileType {
+	case "Restricted":
+		templateFile = "config/templates/resource-policy-restrictive.rego"
+	case "Permissive":
+		templateFile = "config/templates/resource-policy-permissive.rego"
+	default:
+		templateFile = "config/templates/resource-policy-permissive.rego"
+	}
+
+	// Read the template file
+	policyBytes, err := os.ReadFile(templateFile)
+	if err != nil {
+		return "", err
+	}
+
+	return string(policyBytes), nil
+}
