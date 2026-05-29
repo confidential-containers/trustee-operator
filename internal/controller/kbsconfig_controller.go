813d9ed868ba6e7302cc3ba9790658132b4cc156
jack1902 <39212456+jack1902@users.noreply.github.com>
2026-05-29 15:06:47 +0100

    Add admin.personas per trustee repository
    Signed-off-by: jack1902 <39212456+jack1902@users.noreply.github.com>

--- internal/controller/kbsconfig_controller.go
+++ internal/controller/kbsconfig_controller.go
@@ -32,6 +32,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
